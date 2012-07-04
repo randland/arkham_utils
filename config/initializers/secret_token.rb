1b529b8556edbbd0f208f5215c76e41f152fac44
Nick Karpenske <randland@gmail.com>
2012-07-03 23:52:53 -0500

    Generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArkhamUtils::Application.config.secret_token = '4e353659e357494dc06866c655da72632730e85f6af90d192db5dfb7efc9f0b554fabefc20320ff429014f7a6c9f2f2711f24ee62600dddf14b5b3ff63c61e69'

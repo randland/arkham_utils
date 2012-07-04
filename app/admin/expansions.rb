81b68b18aa5ddaf6dbe07221411f31108d3165ff
Nick Karpenske <randland@gmail.com>
2012-07-04 02:55:20 -0500

    Clean Expansion admin index page

--- app/admin/expansions.rb
+++ app/admin/expansions.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Expansion do
-  
+  menu
+
+  index do
+    column '' do |exp|
+      link_to image_tag(exp.icon.url :thumb), admin_expansion_path(exp) if exp.icon.present?
+    end
+    column 'Name', sortable: :name do |exp|
+      link_to exp.name, admin_expansion_path(exp)
+    end
+    column :year
+    default_actions
+  end
 end

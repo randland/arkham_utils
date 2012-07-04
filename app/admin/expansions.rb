ba3e7ffed351b6c1cb93ab3efb70a7999a59836a
Nick Karpenske <randland@gmail.com>
2012-07-04 03:43:17 -0500

    More expansion admin tweaks

--- app/admin/expansions.rb
+++ app/admin/expansions.rb
@@ -1,8 +1,8 @@
 ActiveAdmin.register Expansion do
-  menu
+  config.clear_sidebar_sections!
 
   index do
-    column '' do |exp|
+    column 'Icon' do |exp|
       link_to image_tag(exp.icon.url :thumb), admin_expansion_path(exp) if exp.icon.present?
     end
     column 'Name', sortable: :name do |exp|
@@ -10,5 +10,15 @@ ActiveAdmin.register Expansion do
     end
     column :year
     default_actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :year
+      f.input :big_box
+      f.input :icon, as: :file, :hint => f.template.image_tag(f.object.icon.url :thumb)
+    end
+    f.buttons
   end
 end
--- app/models/expansion.rb
+++ app/models/expansion.rb
@@ -1,5 +1,5 @@
 class Expansion < ActiveRecord::Base
-  attr_accessible :big_box, :name, :year
+  attr_accessible :big_box, :name, :year, :icon
 
   has_attached_file :icon, styles: { thumb: '24x20>' }
 end

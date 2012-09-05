f05340cdca74cd5b84183874c1448a8534fd7db7
Nick Karpenske <randland@gmail.com>
2012-09-05 03:54:34 -0500

    Allow expansion selection on Neighborhood admin form

--- app/admin/neighborhoods.rb
+++ app/admin/neighborhoods.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Neighborhood do
-  
+  index do
+    column 'Expansion' do |inv|
+      if inv.expansion.present?
+        link_to image_tag(inv.expansion.icon.url(:thumb),
+                          title: inv.expansion.name),
+                admin_expansion_path(inv.expansion)
+      end
+    end
+    column :name
+    default_actions
+  end
 end

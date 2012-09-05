23598f49cae558894937710d5018aac41a744e06
Nick Karpenske <randland@gmail.com>
2012-09-05 00:52:11 -0500

    Relocate expansion icons

--- app/models/expansion.rb
+++ app/models/expansion.rb
@@ -1,7 +1,13 @@
 class Expansion < ActiveRecord::Base
   attr_accessible :big_box, :name, :year, :icon
-  has_attached_file :icon, styles: { thumb: '24x20>' }
+
+  has_attached_file :icon, styles: { thumb: '24x20>' },
+    path: 'public/system/:class/:attachment/:style/:filename',
+    url: '/system/:class/:attachment/:style/:basename.:extension'
+
   has_many :investigators
+
   validates_presence_of :name, :year
   validates_uniqueness_of :name
+
 end
